63d24ddb5e0a53d76cbe18cf67a695a3a5b07284
m10090 <49003734+m10090@users.noreply.github.com>
2023-05-23 15:00:12 +0000

    done

--- joins.sql
+++ joins.sql
@@ -3,6 +3,8 @@ use School;
 select *, s.id as s_id from Students s inner join `Attendance` on s.id = `Attendance`.`student_id`;
 select *, s.id as s_id from Students s left join `Attendance` on s.id = `Attendance`.`student_id`;
 select *, s.id as s_id from Students s right join `Attendance` on s.`id` = `Attendance`.`student_id`;
-select *, s.id as s_id from Students s full join `Attendance` on s.`id` = `Attendance`.`student_id`;
--- Carcartesian product
+(select *, Students.id as s_id from Students left join `Attendance` on Students.`id` = `Attendance`.`student_id`)
+union
+(SELECT *, Students.id as s_id from Students  right join `Attendance` on Students.`id` = `Attendance`.`student_id`) ;
+ -- Carcartesian product
 select *, s.id as s_id from Students s cross join `Attendance`; 
